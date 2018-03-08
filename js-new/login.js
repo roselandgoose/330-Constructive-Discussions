83238d04efdef35f3f311731956d1d570136785a
Keith R Languet <krlanguet@gmail.com>
2018-03-08 15:37:55 -0600

    Outlined js functionality

---
+++ js-new/login.js
@@ -0,0 +1,20 @@
+/* Site Loading */
+// On document load, check the login status, set the profile icon display status, and set the log in/out status
+// document.onload
+
+/* Logging In */
+// On clicking the log in button, show the dialogue box
+
+// On pressing enter in the box, click the submit button
+
+// On correct submission, store the current user, display the profile icon, set the log in/out status, and display the reply buttons
+
+// -- If submission is incorrect, notify the user
+
+/* Logging Out */
+// On clicking the log out button, store the user state, undisplay the profile icon, set the log in/out status, and hide the reply buttons
+
+// -- If the user is at their profile page, go to the home page
+
+/* Profile Creation */
+// On clicking the submit button, go to the profile page and log in
